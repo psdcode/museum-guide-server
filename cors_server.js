81c6642b28c7467f25fb0c3cd8b06168e4bd8dd6
Effluxation <patrick.dorosz@gmail.com>
2018-04-15 15:01:35 -0400

    Working locally, no originWhitelist set yet

---
+++ cors_server.js
@@ -0,0 +1,15 @@
+const host = process.env.HOST || '0.0.0.0';
+
+const port = process.env.PORT || 8080;
+
+const corsAnywhere = require('cors-anywhere');
+
+const corsServer = corsAnywhere.createServer({
+  // originWhitelist: ['https://psdcode.github.io'],
+  removeHeaders: ['cookie', 'cookie2'],
+  requireHeader: ['origin', 'x-requested-with']
+});
+
+corsServer.listen(port, host, function () {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
