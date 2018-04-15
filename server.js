5380eb4eed4b9dcf7b744b6eaecd1d8dadeeaba5
Effluxation <patrick.dorosz@gmail.com>
2018-04-15 15:48:15 -0400

    Add originWhitelist option

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const corsAnywhere = require('cors-anywhere');
 
 const corsServer = corsAnywhere.createServer({
-  // originWhitelist: ['https://psdcode.github.io'],
+  originWhitelist: ['https://psdcode.github.io'],
   removeHeaders: ['cookie', 'cookie2'],
   requireHeader: ['origin', 'x-requested-with']
 });
